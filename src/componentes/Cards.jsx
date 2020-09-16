6812af6aeefdb1a7d3b05a542bfde4afa6ad7965
ValentinaCaicedo <valentinacr2567@gmail.com>
2020-09-16 12:16:19 -0500

    Actualizaciones

--- src/componentes/Cards.jsx
+++ src/componentes/Cards.jsx
@@ -9,33 +9,33 @@ class Cards extends React.Component{
         <div className='Cards'>
         <div className="card-deck">
           <div class="card">
-            <img src="..." class="card-img-top" alt="..." />
+            <img src="https://anthoncode.com/wp-content/uploads/2020/02/logo-word-office.png?v=1581371183" class="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 className="card-title">Word</h5>
+              <p className="card-text">Microsoft Word es un procesador de textos que forma parte de un paquete llamado Office. Es en la actualidad, la solución ofimática más utilizada a nivel administrativo junto con otra herramienta del paquete Office: la hoja de cálculo Microsoft Excel.</p>
             </div>
             <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted"></small>
             </div>
           </div>
           <div className="card">
-            <img src="..." class="card-img-top" alt="..." />
+            <img src="https://anthoncode.com/wp-content/uploads/2020/02/logo-power-point.png" class="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <h5 className="card-title">PowerPoint</h5>
+              <p className="card-text">Es un programa diseñado para hacer presentaciones con texto esquematizado, así como presentaciones en diapositivas, animaciones de texto e imágenes prediseñadas o importadas desde imágenes de la computadora. Se le pueden aplicar distintos diseños de fuente, plantilla y dibujos.</p>
             </div>
             <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted"></small>
             </div>
           </div>
           <div className="card">
-            <img src="..." class="card-img-top" alt="..." />
+            <img src="https://anthoncode.com/wp-content/uploads/2020/02/logo-excel-office.png?v=1581371224" class="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <h5 className="card-title">Excel</h5>
+              <p className="card-text">Excel es un programa informático desarrollado y distribuido por Microsoft Corp. Se trata de un software que permite realizar tareas contables y financieras gracias a sus funciones, desarrolladas específicamente para ayudar a crear y trabajar con hojas de cálculo.</p>
             </div>
             <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted"></small>
             </div>
           </div>
         </div>
--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -7,7 +7,7 @@ class Navbar extends React.Component{
       <div>
         <div className="Navbar">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className='container'>
+        <div className='container'>
           <a className="navbar-brand" href="#">Ofilmax</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
