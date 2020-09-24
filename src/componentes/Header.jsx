1a29f82eb264cffd58071adb8f41c313a43b2328
ValentinaCaicedo <valentinacr2567@gmail.com>
2020-09-23 19:25:23 -0500

    Rutas

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -8,7 +8,7 @@ class Header extends React.Component{
         <div>
             <div className="Header">
         <div className="card mb-3">
-            <img src= "..." class="card-img-top" alt=".." />
+            <img src= "" class="card-img-top" alt=".." />
             <div className="card-body">
                 <h5 className="card-title">Herraientas Ofilmaticas</h5>
                 <p className="card-text">Las herramientas de ofimática son un conjunto de técnicas, aplicaciones y programas informáticos que se utilizan en funciones de oficina para optimizar, automatizar y mejorar los procedimientos y tareas relacionados.
--- src/pagina/Inicio.jsx
+++ src/pagina/Inicio.jsx
@@ -1,36 +1,53 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import '../imagenes/Logo.png';
 
 class Inicio extends React.Component {
     render() {
         return (
-            <div>
-                <div className="dropdown-menu">
-                    <form className="px-4 py-3">
-                        <div className="form-group">
-                            <label for="exampleDropdownFormEmail1">Email address</label>
-                            <div type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com" />
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleDropdownFormPassword1">Password</label>
-                            <div type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password" />
-                        </div>
-                        <div className="form-group">
-                            <div className="form-check">
-                                <div type="checkbox" class="form-check-input" id="dropdownCheck" />
-                                <label className="form-check-label" for="dropdownCheck">
-                                    Remember me </label>
-                            </div>
-                        </div>
-                        <Link to= "/" button type="submit" className="btn btn-primary">Iniciar</Link>
-                    </form>
-                    <div class="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">New around here? Sign up</a>
-                    <a className="dropdown-item" href="#">Forgot password?</a>
-                </div>
-            </div >
+          
+            <div className="container vh-100  mt-4">
+            <div className="row container vh-100  mt-4">
+              <div className="col"></div>
+              <div className="col-6 container vh-100  mt-4 text-center">
+              <img src="https://lh6.googleusercontent.com/FoPE60pJOnPHvaRyJgi5ZDgs7MwAjlZENNYDt1YC_keGmc0imxeNn5-n0VnvnTChRqTcZCG3xYNAGY_LjBuqjjQdfrJ7i6NkSzfcvavbWUXZCJyXLsU=w1280" className="rounded" alt="" width= "170" height="160"/>
+                <h5 className="text-center mt-4">Iniciar Sesión</h5>
+                <br />
+                <form >
+                  <div className="form-group">
+                    <label className="inputEmail">Correo Electronico</label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="inputEmail"
+                      className="form-control"
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Contraseña</label>
+                    <input
+                      type="password"
+                      name="contrasena"
+                      id="inputPassword"
+                      className="form-control"
+                      required
+                    />
+                  </div>
+                  <Link to="/" button className=" btn btn-orange btn btn-info btn-block" type="submit">
+                    <strong>Iniciar Sesión</strong>
+                  </Link>
+                </form>
+                <br />
+                <h6>¿No tienes una cuenta? Registrate</h6>
+                <h6>Olvide mi Contraseña</h6>
+                <br />
+              </div>
+              <div className="col"></div>
+            </div>
+          </div>
         );
+      }
     }
-}
-
+      
 export default Inicio;
